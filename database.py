e229e60e0b0bb857b21e8cd6a78f14f9267222ba
Preston Werntz <werntzp@hotmail.com>
2014-12-21 22:05:02 -0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+USERNAME = ""
+PASSWORD = ""
+
+login_info = {
+            'host': "sql.oreillyschool.com",
+            'user': USERNAME,
+            'password': PASSWORD,
+            'database': USERNAME,
+            'port': 3306
+            }
